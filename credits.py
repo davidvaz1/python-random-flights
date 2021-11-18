acd27f088fcf50bcd1bc97473ca5e445e6db1b37
davidvaz1 <74098173+davidvaz1@users.noreply.github.com>
2021-11-18 11:11:43 +0530

    added credits to the project

---
+++ credits.py
@@ -0,0 +1,15 @@
+credits = """
+Ascii Art Airplane Title:\n https://www.asciiart.eu/vehicles/airplanes\n
+By Roland\n\n
+
+Airlines:\n
+https://www.seatguru.com/browseairlines\n\n
+
+Code:\n
+Me (@PythonDavid) (replit)\n\n
+
+
+Ascii Art Airplane Instruction Title:\n
+https://www.asciiart.eu/vehicles/airplanes\n
+
+"""
