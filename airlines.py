9d2aff14b256a2720a003a8b73d0c4ff4a67df46
davidvaz1 <74098173+davidvaz1@users.noreply.github.com>
2021-11-18 11:04:42 +0530

    added airlines

---
+++ airlines.py
@@ -0,0 +1,1 @@
+airlines = ['Aer Lingus', 'Belair', 'Aerolineas Sosa', 'Emery Worldwide', 'Fine Air', 'TruJet', 'EasyJet','Cathay Pacific','GoAir','Air India','Aegean Airlines','Aeroflot','Aerolineas Argentinas','Aeromexico','Air Arabia','SpiceJet','Bamboo Airways','Spirit Airlines','Frontier Airlines','Hawaiian Airlines','	Allegiant Air']
